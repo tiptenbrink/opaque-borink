3d11830b137208439fb348e83bdc1b2808e37a43
Tip ten Brink <75669206+tiptenbrink@users.noreply.github.com>
2022-10-02 23:39:18 +0200

    Full typing

--- opaquepy/python/opaquepy/__init__.py
+++ opaquepy/python/opaquepy/__init__.py
@@ -1,1 +1,9 @@
-from .lib import create_setup, register, register_finish, login, login_finish, register_client, register_client_finish
+from opaquepy.lib import (
+    create_setup,
+    register,
+    register_finish,
+    login,
+    login_finish,
+    register_client,
+    register_client_finish
+)
